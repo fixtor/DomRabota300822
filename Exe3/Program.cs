167b004f3654d5f581c0633aeb2d1c235d6701a9
Igor_U <fixtor75@gmail.com>
2022-08-31 16:29:38 +0300

    Задача 58 Готова

---
+++ Exe3/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] GetMatrixOne(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int[,] GetMatrixTwo(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+            Console.Write($"{inArray[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MultiplyMatrix(int[,] matrixOne, int[,] matrixTwo)
+{
+    int[,] array = new int[matrixOne.GetLength(0), matrixTwo.GetLength(1)];
+    for (int i = 0; i < matrixOne.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixTwo.GetLength(1); j++)
+        {
+            for (int n = 0; n < matrixTwo.GetLength(0); n++)
+                array[i, j] = array[i,j] + (matrixOne[i, n] * matrixTwo[n, j]);
+        }
+    }
+    return array;
+}
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrixOne = GetMatrixOne(rows, columns, 0, 10); //вводим одинаковые размеры матриц 1,2
+int[,] matrixTwo = GetMatrixTwo(rows, columns, 0, 10);
+int[,] multiply = MultiplyMatrix(matrixOne, matrixTwo); // умножаем
+
+PrintArray(matrixOne);
+PrintArray(matrixTwo);
+PrintArray(multiply);
