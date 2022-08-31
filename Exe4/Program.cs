2c81f592fddb1663897e227a558f9ca13d5e713e
Igor_U <fixtor75@gmail.com>
2022-08-31 19:48:00 +0300

    Задача 60 Готова

---
+++ Exe4/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Clear();
+
+int[,,] GetArray(int x, int y, int z)
+{
+    int[,,] result = new int[x, y, z];
+    int doubleSizeNumber = 100;
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < result.GetLength(2); k++)
+            {
+                result[i, j, k] = new Random().Next(doubleSizeNumber - 9, doubleSizeNumber);
+                doubleSizeNumber = doubleSizeNumber - 10;
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+                Console.Write($"{inArray[i, j, k]} ({i},{j},{k}) ");
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Clear();
+int[,,] someArray = GetArray(2, 2, 2);
+PrintArray(someArray);
+
